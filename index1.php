2e58997a502483f30f8b00b08eb5af5ff64a3fc9
sajjad <sajjadmemon205@gmail.com>
2025-07-29 18:32:25 +0530

    done

--- index1.php
+++ index1.php
@@ -1,5 +1,24 @@
-<?php
-echo"<pre>";
-print_r($_POST);
-echo"</pre>";
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>Form Data Display</title>
+</head>
+<body>
+  <h2>Submitted Information</h2>
+
+  <?php
+  if ($_SERVER["REQUEST_METHOD"] == "POST") {
+      echo "First Name: " . htmlspecialchars($_POST["first_name"]) . "<br><br>";
+      echo "Last Name: " . htmlspecialchars($_POST["last_name"]) . "<br><br>";
+      echo "Email: " . htmlspecialchars($_POST["email"]) . "<br><br>";
+      echo "Password: " . htmlspecialchars($_POST["password"]) . "<br><br>";
+      echo "Phone Number: " . htmlspecialchars($_POST["phone"]) . "<br><br>";
+      echo "Gender: " . htmlspecialchars($_POST["gender"]) . "<br><br>";
+  } 
+  else {
+      echo "No data submitted.";
+  }
+  ?>
+</body>
+</html>
