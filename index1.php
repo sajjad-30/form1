0da8df86a489f43a07cd20460707a313f8d83e41
sajjad <sajjadmemon205@gmail.com>
2025-07-29 13:04:41 +0530

    file

--- index1.php
+++ index1.php
@@ -1,38 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Simple Form</title>
-</head>
-
-<body>
-    <form action="index.php" method="post">
-        <label for="fname">First Name:</label>
-        <input type="text" id="fname" name="first_name"><br><br>
-
-        <label for="lname">Last Name:</label>
-        <input type="text" id="lname" name="last_name"><br><br>
-
-        <label for="email">Email:</label>
-        <input type="email" id="email" name="email"><br><br>
-
-        <label for="password">Password:</label>
-        <input type="password" id="password" name="password"><br><br>
-
-        <label for="phone">Phone Number:</label>
-        <input type="number" id="phone" name="phone"><br><br>
-
-        <label>Gender:</label>
-        <input type="radio" id="male" name="gender" value="male">
-        <label for="male">Male</label>
-
-        <input type="radio" id="female" name="gender" value="female">
-        <label for="female">Female</label><br><br>
-
-        <input type="submit" value="Submit">
-    </form>
-</body>
-
-</html>
+<?php
+echo"<pre>";
+print_r($_POST);
+echo"</pre>";
+?>
